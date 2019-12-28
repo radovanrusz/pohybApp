be5f6a62b6601288936717e02b8b094ebbecde24
Radovan Rusz <radovan.rusz@trz.cz>
2019-12-28 17:02:45 +0100

    connect to Kafka

--- src/index.js
+++ src/index.js
@@ -1,20 +1,74 @@
 /* eslint-disable no-console */
+const kafkaHost = process.env.KAFKA_HOST;
+const kafkaPort = process.env.KAFKA_PORT;
+const kafkaTopic = process.env.KAFKA_TOPIC;
 const express = require('express');
 require('./db/mongoose');
+const kafka = require('kafka-node');
 const Pohyb = require('./models/pohyb');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json())
+app.use(express.json());
 
 
-app.post('/pohyby', (req, res)=>{
+let mDate = new Date();
+let mDateStr = mDate.toString('dddd MMM yyyy h:mm:ss');
+
+try {
+  console.log(`${mDateStr}: Kafka Consumer is booting up ... (ENVs: kafkaHost:${kafkaHost}; kafkaTopic:${kafkaTopic}; kafkaHost:${kafkaHost}; kafkaPort:${kafkaPort};)`);
+
+  const client = new kafka.KafkaClient({ kafkaHost: `${kafkaHost}:${kafkaPort}` });
+  const topics = [
+    {
+      topic: kafkaTopic,
+      partition: 0 
+    }
+  ];
+  const options = {
+  // autoCommit: true,
+    autoCommit: false,
+    fetchMaxWaitMs: 1000,
+    fetchMaxBytes: 1024 * 1024,
+    encoding: 'utf8',
+    fromOffset: false,
+  };
+
+  const consumer = new kafka.Consumer(client, topics, options);
+  consumer.setOffset(kafkaTopic, 0, 0);
+
+  client.on('ready', () => {
+    console.log('Client ready!');
+  });
+
+  consumer.on('offsetOutOfRange', (err) => {
+    console.log(`Kafka offsetOutOfRange: ${err}`);
+  });
+
+  consumer.on('message', async (message) => {
+    mDate = new Date();
+    mDateStr = mDate.toString('dddd MMM yyyy h:mm:ss');
+    console.log(`${mDateStr} : consumer.on() invoked.`);
+    console.log(JSON.parse(message.value));
+  });
+
+  consumer.on('error', (err) => {
+    mDate = new Date();
+    mDateStr = mDate.toString('dddd MMM yyyy h:mm:ss');
+    console.log(`${mDateStr} : Consumer on error ${err}`);
+  });
+} catch (e) {
+  console.log(e);
+}
+
+app.post('/pohyby', (req, res) => {
   const pohyb = new Pohyb(req.body);
-  pohyb.save().then(()=>{
+  pohyb.save().then(() => {
     res.send(pohyb);
-  }).catch((error)=>{res.status(500).send(error);
-  })
+  }).catch((error) => {
+    res.status(500).send(error);
+  });
 });
 
 app.get('/pohyby', (req, res) => {
@@ -22,22 +76,19 @@ app.get('/pohyby', (req, res) => {
   console.log(req.query);
   // eslint-disable-next-line prefer-template
   const params = {};
-  if (typeof mvm1 !== 'undefined')
-  {
-    params.mvm1 = { $regex: '.*' + mvm1 + '.*'};
-  }
-  if (typeof kmat !== 'undefined')
-  {
-    params.kmat = { $regex: '.*' + kmat + '.*' };
+  if (typeof mvm1 !== 'undefined') {
+    params.mvm1 = { $regex: `.*${mvm1}.*`};
   }
 
-  if (typeof mnozstvi !== 'undefined')
-  {
+  if (typeof kmat !== 'undefined') {
+    params.kmat = { $regex: `.*${kmat}.*` };
+  }
+
+  if (typeof mnozstvi !== 'undefined') {
     params.mnozstvi = { $eq: mnozstvi };
   }
 
-  if (typeof hmotnost !== 'undefined')
-  {
+  if (typeof hmotnost !== 'undefined') {
     params.hmotnost = { $eq: hmotnost };
   }
 
