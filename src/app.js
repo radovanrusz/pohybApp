0f0f16f0efb90a6f7b07b7d63026ed7ecdce5cc5
Radovan Rusz <radovan.rusz@trz.cz>
2020-01-13 15:54:11 +0100

    nastaveni CORS policy

--- src/app.js
+++ src/app.js
@@ -5,6 +5,22 @@ const router = require('./routers/router');
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Cache-Control, Pragma, Origin, '
+    + 'Authorization, Content-type, X-Requested-With',
+  );
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+
 app.use(express.json());
 app.use(router);
 
